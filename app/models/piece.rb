13c32092e015075d340b3d4f7949fbaf3f42569d
morganric <morganric@gmail.com>
2014-10-15 15:57:55 +0100

    assets

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -4,6 +4,13 @@ class Piece < ActiveRecord::Base
 
 	mount_uploader :image, ImageUploader
 
+	validate :maximum_amount_of_tags
+	
+	def maximum_amount_of_tags
+	number_of_tags = tag_list_cache_on("tags").uniq.length
+	errors.add(:base, "Please only add 5 tags") if number_of_tags > 5
+	end
+
 	belongs_to :user
 
 	extend FriendlyId
