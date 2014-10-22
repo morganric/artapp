da67d5aaf9dd0260173289e380deec0b90833252
morganric <morganric@gmail.com>
2014-10-22 15:52:37 +0100

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
   # end
 end
 
- # get 'pages/:id' => 'high_voltage/pages#show', as: 'page'
+ get 'pages/:id' => 'high_voltage/pages#show', as: 'about'
 
   resources :relationships,       only: [:create, :destroy]
 
