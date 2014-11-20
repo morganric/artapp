18d13fd722a24614019e2213e9d488207414f86d
morganric <morganric@gmail.com>
2014-11-20 17:56:06 +0000

    categories

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -23,7 +23,7 @@ class PiecesController < ApplicationController
   # end
 
   def categories
-    @tags = Piece.tag_counts_on(:tags).order("taggings_count DESC").limit(6)
+    @tags = Piece.tag_counts_on(:tags).order("taggings_count DESC").limit(7)
   end 
 
 
