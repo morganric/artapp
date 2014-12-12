b9ed427f4be990d7776eada1549bfe53fdfbfb36
morganric <morganric@gmail.com>
2014-12-12 12:24:54 +0000

    this

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,6 +1,6 @@
 class Users::InvitationsController < Devise::InvitationsController
 
-  prepend_before_filter :authenticate_inviter!, :only => [:new, :create]
+  # prepend_before_filter :authenticate_inviter!, :only => [:new, :create]
   prepend_before_filter :has_invitations_left?, :only => [:create]
   prepend_before_filter :require_no_authentication, :only => [:edit, :update, :destroy]
   prepend_before_filter :resource_from_invitation_token, :only => [:edit, :destroy]
