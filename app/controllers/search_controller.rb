778cdc0b42c6bdba07f2185fdd9cbba58818c34a
morganric <morganric@gmail.com>
2015-01-09 23:06:25 +0000

    title

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,9 +7,11 @@ def index
 	if params[:q]
 	@pieces = Piece.where(:title => params[:q]).limit(10)
 	@profiles = Profile.where(:display_name => params[:q]).limit(10)
+	@tags = Piece.tagged_with(params[:q], :any => true).limit(10)
 	else
 		@pieces = []
 		@profiles = []
+		@tags = []
 	end
 
 end
