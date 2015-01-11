6e23aea53a732e3433eeea51bc942ef243a16f91
morganric <morganric@gmail.com>
2015-01-11 19:44:33 +0000

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def url_with_protocol(url)
     /^http/i.match(url) ? url : "http://#{url}"
   end
+
+  
+	def pieces(category)
+		@category = category
+		pieces = []
+		@category.users.each do |user|
+			pieces << user.pieces.last
+		end
+
+		return pieces.sort.reverse
+	end
 end
